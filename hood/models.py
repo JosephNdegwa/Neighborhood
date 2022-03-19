7041f3c17c12b267fe90f0ecee4ee945e8dcc029
josephndegwa <kariukindegwa98@gmail.com>
2022-03-19 12:57:40 +0300

    create business class model

--- hood/models.py
+++ hood/models.py
@@ -8,8 +8,17 @@ class Location(models.Model):
 
 
 class Neighbourhood(models.Model):
-    neighbourhood_photo = models.ImageField(upload_to='neighbourhood/')
+    neighbourhood_photo = models.ImageField(upload_to='neighborhood/')
     neighbourhood_name = models.CharField(max_length=100, blank=True, null=True)
     occupants_count = models.IntegerField(blank=True, null=True)
     neighbourhood_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
     admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+
+
+class Business(models.Model):
+    business_photo = models.ImageField(upload_to='business/')
+    business_name = models.CharField(max_length=100, blank=True, null=True)
+    business_description = models.TextField(max_length=200, blank=True, null=True)
+    business_email = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
