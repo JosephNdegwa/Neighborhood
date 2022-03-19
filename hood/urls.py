fbe1e65242b873aafbb15dea8baa19199e90259c
josephndegwa <kariukindegwa98@gmail.com>
2022-03-19 12:51:22 +0300

    update base url links

--- hood/urls.py
+++ hood/urls.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from django.conf.urls import path
+from django.urls import path
 from django.conf.urls.static import static
 
 from hood import views
 
 urlpatterns=[
-    path('', views.welcome, name='homepage'),
+    path('', views.homepage, name='homepage'),
 
 
 ]
--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    title = 'Neighborhood'
+    return render(request, 'homepage.html', locals())
