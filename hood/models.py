fa894db6815a644f6d4492dd05dc7917dcc94588
josephndegwa <kariukindegwa98@gmail.com>
2022-03-19 12:56:12 +0300

    updatemodels

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Location(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+
+class Neighbourhood(models.Model):
+    neighbourhood_photo = models.ImageField(upload_to='neighbourhood/')
+    neighbourhood_name = models.CharField(max_length=100, blank=True, null=True)
+    occupants_count = models.IntegerField(blank=True, null=True)
+    neighbourhood_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
