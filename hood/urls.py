8b0b5898c53855dd6659298cc4bad7c12ff0671a
josephndegwa <kariukindegwa98@gmail.com>
2022-03-19 12:36:50 +0300

    update urls in neiba for homepage

---
+++ hood/urls.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.conf.urls import path
+from django.conf.urls.static import static
+
+from hood import views
+
+urlpatterns=[
+    path('', views.welcome, name='homepage'),
+
+
+]
+if settings.DEBUG:
+     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
