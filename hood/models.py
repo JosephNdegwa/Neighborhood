efe671a0a113bc147009eabd0e956669b2b06c66
josephndegwa <kariukindegwa98@gmail.com>
2022-03-20 17:12:35 +0300

    update profile class

--- hood/models.py
+++ hood/models.py
@@ -3,14 +3,14 @@ from django.db import models
 
 # Create your models here.
 
-class Location(models.Model):
+class Post(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
 
 
 class Neighborhood(models.Model):
     neighbourhood_name = models.CharField(max_length=100, blank=True, null=True)
     occupants_count = models.IntegerField(blank=True, null=True)
-    neighbourhood_location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True)
+    neighbourhood_location = models.ForeignKey(Post, on_delete=models.CASCADE, blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
@@ -30,13 +30,24 @@ class Business(models.Model):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
     bio = models.TextField(max_length=500, blank=True)
     email = models.EmailField(max_length=200)
-    neighborhood_key = models.ForeignKey(Neighborhood, on_delete=models.CASCADE, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
     def __str__(self):
-        return self.user.name
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 
     
