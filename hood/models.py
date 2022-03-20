fa8bb29679394386ed3bc7771b18cc071c3df367
josephndegwa <kariukindegwa98@gmail.com>
2022-03-20 18:13:23 +0300

    update neighborhood class

--- hood/models.py
+++ hood/models.py
@@ -4,6 +4,46 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 # Create your models here.
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+    occupants_count = models.IntegerField(blank=True, null=True)
+    neighbourhood_location = models.ForeignKey(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
+    email = models.EmailField(max_length=200)
+    neighbourhood_location = models.CharField(max_length=50, blank=True, null=True)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 class Post(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
@@ -41,25 +81,7 @@ class Business(models.Model):
     #update_business()
 
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    name = models.CharField(max_length=80, blank=True)
-    bio = models.TextField(max_length=500, blank=True)
-    email = models.EmailField(max_length=200)
-    location = models.CharField(max_length=50, blank=True, null=True)
-    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
 
-    def __str__(self):
-        return f'{self.user.username} profile'
-
-    @receiver(post_save, sender=User)
-    def create_user_profile(sender, instance, created, **kwargs):
-        if created:
-            Profile.objects.create(user=instance)
-
-    @receiver(post_save, sender=User)
-    def save_user_profile(sender, instance, **kwargs):
-        instance.profile.save()
 
 
     
