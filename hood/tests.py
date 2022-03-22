b8665c46d66934a243be6193742fb1e91de73aac
josephndegwa <kariukindegwa98@gmail.com>
2022-03-22 03:31:36 +0300

    add a test for profile

--- hood/tests.py
+++ hood/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Profile, Business, Neighborhood
 
-# Create your tests here.
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        self.profile = User.objects.create(username="ken")
+    
+    def test_proxy_user(self):
+        self.profile.bio = ""
+        self.assertEqual(self.profile.bio, "")
+
+class BusinessTestCase(TestCase):
+    def setUp(self):
+        self.owner = User.objects.create(username="joseph")
+        self.business = Business.objects.create(owner=self.owner, business=self.business)
+
+    def test_follow(self):
+        self.assertEqual(self.business.owner, self.owner)
+        self.assertEqual(self.business.business, self.business)
+
+
